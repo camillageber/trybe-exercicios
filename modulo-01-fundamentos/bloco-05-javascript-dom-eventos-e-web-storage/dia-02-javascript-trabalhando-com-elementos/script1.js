86c733d363b5605789b47f1e23523ab6a41c81f4
Camilla Geber <camilla.geber@gmail.com>
2022-02-19 09:09:40 -0300

    Adiciona exercicios 'Removendo Elementos'.

--- modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script1.js
+++ modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script1.js
@@ -49,3 +49,20 @@ const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
    .parentElement // elementoOndeVoceEsta
    .nextElementSibling; // terceiroFilho
  console.log(terceiroFilho);
+
+// -------------------------------------------
+// EXERCICIOS PARTE III - Removendo Elementos
+
+// Remova todos os elementos filhos de paiDoPai exceto pai,
+    // elementoOndeVoceEsta e primeiroFilhoDoFilho.
+    const pai = document.getElementById('pai');
+
+    for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+      const currentChildren = pai.childNodes[index];
+      if (currentChildren.id !== 'elementoOndeVoceEsta') {
+        currentChildren.remove();
+      }
+    }
+
+    const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+    segundoEUltimoFilhoDoFilho.remove();
