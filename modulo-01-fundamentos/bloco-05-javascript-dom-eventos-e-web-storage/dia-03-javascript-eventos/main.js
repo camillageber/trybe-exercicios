d7f43a2271587ccf04a82d2d612a4f53ef1752f8
Camilla Geber <camilla.geber@gmail.com>
2022-02-22 13:14:15 -0300

    Adiciona exercicio 1 - calendar

--- modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
+++ modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
@@ -8,6 +8,8 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// Resposta: Ocorre por causa da propriedade transform aplicada a classe .tech no documento CSS.
+// Essa propriedade, junto ao valor translateY faz com que a caixa suba um pouco em relação ao eixo Y. 
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
