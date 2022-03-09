b9524086a2a84c80efb03bab492a037b23a35295
Camilla Geber <camilla.geber@gmail.com>
2022-03-09 20:46:16 -0300

    Adiciona exercicio 1 da parte II, dia 7.2

---
+++ modulo-01-fundamentos/bloco-07-introducao-ajavascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -0,0 +1,64 @@
+// PARTE II
+
+// EXERCÍCIO 1:
+// Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+// Note que o parâmetro da função já está sendo passado na chamada da função.
+
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+// OBS: Foi necessário criar uma arrow function contendo as informações do pedido contidos no objeto order, 
+// como as informações do cliente, do entregador e o que o cliente pediu. Para puxar essas informações
+// fiz o uso de variáveis, contendo os dados. A const address, por puxar todo o subobjeto address, foi
+// declarada com o valor 'address', para puxar todos os parâmetros de seu subobjeto. 
+// Em algumas consts, declarei com notação de ponto e outras com  conchetes, ambas funcionam. 
+// Após isso, foi necessário criar um console.log (ainda dentro da função), usando template literals para conter a frase pedida 
+// no exercício, utilizando as variáveis criadas na função para completar os dados. 
+// Depois, chamei a função, usando como parâmetro o meu objeto de referência (order).
+
+  const customerInfo = (pedido) => {
+  const address = 'address';
+  const deliveryPerson = pedido.order.delivery.deliveryPerson;
+  const customerName = pedido.name;
+  const customerPhone = pedido['phoneNumber'];
+  const street = pedido[address].street;
+  const number = pedido[address].number;
+  const apartment = pedido.address.apartment;
+
+  console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+}
+
+customerInfo(order);
