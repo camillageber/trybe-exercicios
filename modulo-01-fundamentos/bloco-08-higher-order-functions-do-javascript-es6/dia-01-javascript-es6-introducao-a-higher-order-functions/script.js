8312c066318e837778dee93f6fa74c3da986bb97
Camilla Geber <camilla.geber@gmail.com>
2022-03-15 02:39:17 -0300

    Adiciona exercicio 2 do dia 8.1

--- modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/script.js
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -26,3 +26,30 @@ console.log(newEmployees(createEmail));
 // 4. A função que você criará recebe como parâmetro o nome completo
 // 5. A partir desse nome completo, o retorno deve gerar automaticamente um e-mail nome_da_pessoa@trybe.com
 
+// ------------------------------------------------------------------------------------------------------------
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+
+function checkResult(numberApostado, numberSorteado) {
+  return numberApostado === numberSorteado;
+};
+
+function sortResult(numberApostado, checkResult) {
+  const luckNumber = Math.floor(Math.random() * 5) + 1;
+  console.log(luckNumber);
+  if (numberApostado === luckNumber) {
+    return 'Parabéns, você ganhou';
+  } if (numberApostado !== luckNumber) {
+    return 'Tente novamente';
+  }
+  return checkResult(numberApostado, luckNumber) 
+};
+console.log(sortResult(4, checkResult));
+
+// Baby steps: 
+// 1. Desenvolver uma HOF 
+// 2. Essa Hof retorna o resultado de um sorteio
+// 3. Esse sorteio irá gerar um número aleatório entre 1 e 5
+// 4. A HOF recebe como parametros o número apostado e outra função
+// 5. A outra função checa e retorna se o número apostado é igual ao sorteado
+// 6. o retorno da HOF é uma string "Tente novamente" ou "Parabéns você ganhou"
